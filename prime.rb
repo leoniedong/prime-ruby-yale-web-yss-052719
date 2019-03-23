357c40d296e25957bfcdf3721f1a283cc80815d4
Leonie Dong <leoniedong@hotmail.com>
2019-03-23 15:35:13 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,9 @@
-# Add  code here!
+def prime?(integer)
+  number = [2..integer]
+  if interger < 0
+    puts false
+  elsif number.each {|i| interger % i != 0 }
+    puts true
+  else
+    puts false
+  end
