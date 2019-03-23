0738d7e016195359cd163b2d540032b3235f1284
Leonie Dong <leoniedong@hotmail.com>
2019-03-23 15:34:52 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,3 +1,9 @@
 def prime?(integer)
-  number = [1..infinity]
-  if inter
+  number = [2..integer]
+  if interger < 0
+    puts false
+  elsif number.each {|i| interger % i != 0 }
+    puts true
+  else
+    puts false
+  end
